90b5b949d85ea1fc0844c7ce1966586b251beedf
Milaimac <1099843695@qq.com>
2021-04-18 12:22:00 +0800

    for test

---
+++ test.cpp
@@ -0,0 +1,7 @@
+#include" Longest_Increasing_Subsequence.h"
+int  main(){
+    Solution sl;
+    vector<int> ivec = {1,2,3,4,2,3,9};
+    int res = sl.lengthOfLIS(ivec);
+    cout << res << endl;
+}
