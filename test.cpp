afef50e8c2a698b8b6e3c885c5f993362bb1fd5f
Milaimac <1099843695@qq.com>
2021-04-19 16:30:40 +0800

    for test

--- test.cpp
+++ test.cpp
@@ -1,7 +1,8 @@
-#include"Longest_Increasing_Subsequence.h"
+// #include"Longest_Increasing_Subsequence.h"
+#include"Maximum_Product_Subarray.h"
 int  main(){
     Solution sl;
-    vector<int> ivec = {1,2,3,4,2,3,9};
-    int res = sl.lengthOfLIS(ivec);
+    vector<int> ivec = {-2,0,-1};
+    int res = sl.maxProduct1(ivec);
     cout << res << endl;
 }
