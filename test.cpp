f159aa849c701a996e1ea43c1142f11d1289d6ec
Milaimac <1099843695@qq.com>
2021-04-24 14:54:46 +0800

    for test

--- test.cpp
+++ test.cpp
@@ -1,8 +1,30 @@
 // #include"Longest_Increasing_Subsequence.h"
-#include"Maximum_Product_Subarray.h"
-int  main(){
+#include "Max_Sum_of_Rectangle_No_Larger_Than_K.h"
+int main()
+{
     Solution sl;
-    vector<int> ivec = {-2,0,-1};
-    int res = sl.maxProduct1(ivec);
+    vector<vector<int>> ivec = {{2,2,-1}};
+    int res = sl.maxSumSubmatrix_1(ivec, 0);
     cout << res << endl;
+    //cout <<INT_MIN << endl;
+
+    // int n, m;
+    // cin >> n >> m;
+    // vector<vector<int>> dp(n, vector<int>(m, 0));
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < m; j++)
+    //     {
+    //         // cout <<dp[i][j] <<endl;
+    //         dp[i][j] = 2;    
+    //     }
+    // }
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < m; j++)
+    //     {
+    //         cout << dp[i][j] << endl;
+    //         // dp[i][j] =2;
+    //     }
+    // }
 }
