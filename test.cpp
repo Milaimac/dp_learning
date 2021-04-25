959f51e3d3a0369a535cf987bb12ed8ebf115255
Milaimac <1099843695@qq.com>
2021-04-25 15:54:45 +0800

    for test

--- test.cpp
+++ test.cpp
@@ -1,30 +1,11 @@
 // #include"Longest_Increasing_Subsequence.h"
-#include "Max_Sum_of_Rectangle_No_Larger_Than_K.h"
+// #include "Max_Sum_of_Rectangle_No_Larger_Than_K.h"
+#include"Pizza_with_3n_slices.h"
+
 int main()
 {
     Solution sl;
-    vector<vector<int>> ivec = {{2,2,-1}};
-    int res = sl.maxSumSubmatrix_1(ivec, 0);
-    cout << res << endl;
-    //cout <<INT_MIN << endl;
-
-    // int n, m;
-    // cin >> n >> m;
-    // vector<vector<int>> dp(n, vector<int>(m, 0));
-    // for (int i = 0; i < n; i++)
-    // {
-    //     for (int j = 0; j < m; j++)
-    //     {
-    //         // cout <<dp[i][j] <<endl;
-    //         dp[i][j] = 2;    
-    //     }
-    // }
-    // for (int i = 0; i < n; i++)
-    // {
-    //     for (int j = 0; j < m; j++)
-    //     {
-    //         cout << dp[i][j] << endl;
-    //         // dp[i][j] =2;
-    //     }
-    // }
+    vector<int> test = {1,2,3,4,5,6};
+    int res = sl.maxSizeSlices(test);
+    cout <<res;
 }
