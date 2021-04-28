2e38ffd255811759d2279121d4ced942698a0275
Milaimac <1099843695@qq.com>
2021-04-28 22:39:23 +0800

    #include"head.h" class Solution { public:     int numDecodings(string s) {         //该题关键在于，去加一和去继承         //如果说发现能够独立成为一个数字，先让它继承s[i-1]的状态，发现能够和s[i-1]组成小于26的数字，那么再加一         //怎么应对0的打扰         // 直接将s[i-1]和s[i]转换为数字，看看在不在对应的区间上。         int n = s.size();         vector<int> dp(3,0);dp[0] = 1;         s=" " +s;         for(int i=1;i<=n;i++){             int a = s[i]-'0'; int b = a+10*(s[i-1]-'0');             dp[i%3]=0;//用滚动数组要清空之前的状态             if(a>0&&a<=9){                 dp[i%3] += dp[(i-1)%3];                 // cout << dp[i%3] << endl;             }             if(b>=10&&b<=26){                 dp[i%3] += dp[(i-2)%3];                 // cout << dp[i%3]<< " d d" << endl;             }         }         return dp[n%3];     } };

--- Length_of_Longest_Fibonacci_Subsequence.h
+++ Length_of_Longest_Fibonacci_Subsequence.h
@@ -1,6 +1,8 @@
 #include"head.h"
 class Solution {
 public:
+    // 暴力法，对于每个起始对arr[i], arr[j]，我们都去计算y = arr[i] + arr[j]，如果y在数组中，则去跟新，x = y, y = x + y;
+    // 算法复杂度 o(N^2 log(m))
     int lenLongestFibSubseq(vector<int>& arr) {
         int n = arr.size();
         int ans = 0;
@@ -18,5 +20,47 @@ public:
             }
         }
         return ans>=3?ans:0;
+    }
+
+    // 使用动态规划
+    int lenLongestFibSubseq_1(vector<int>& arr){
+        int n = arr.size();
+        unordered_map<int, int> index;
+        for(int i=0;i<n;i++){
+            index[arr[i]] = i;
+        }
+
+        unordered_map<int, int> longest;
+        int ans =0;
+        for(int k=0;k<n;k++){
+            for(int j=0;j<k;j++){
+                if(arr[k]-arr[j] < arr[j] && index.count(arr[k]-arr[j])){
+                    int i = index[arr[k] - arr[j]];
+                    longest[j*n + k] = longest[i*n+j] + 1; //这里使用了哈希的思想， 避免了longest[j+k] = longest[i+j] + 1的哈希冲突。
+                    ans = max(ans, longest[j*n+k] + 2);
+                }
+            }
+            return ans>3?ans:0;
+        }
+    }
+
+    int longestArithSeqLength(vector<int>& A) {
+        //直到后面两个数，就直到前面一个数，target = A[i]*2 - A[j];
+        unordered_map<int,int> mp;
+        int n = A.size();
+        // vector<vector<int>> dp(n, vector<int>(n, 2));
+        unordered_map<int, int> dp;
+        int ans = 0;
+        for(int i=0;i<n;i++){
+            for(int j = i+1;j<n;j++){
+                int target = A[i]*2 - A[j];
+                if(mp.count(target)){
+                    dp[i*n+j]  = dp[mp[target]*n+i] + 1;
+                }
+                ans = max(ans, dp[i*n+j]+2);
+            }
+            mp[A[i]] = i;
+        }
+        return ans;
     }
 };
